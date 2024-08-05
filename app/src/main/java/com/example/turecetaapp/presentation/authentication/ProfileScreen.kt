3c83a73c8e0a90fdf271dedf6b91aa2a19414bd9
Stronauta <samircastillop@gmail.com>
2024-08-05 02:37:32 -0400

    Creando pantalla para el usuario

---
+++ app/src/main/java/com/example/turecetaapp/presentation/authentication/ProfileScreen.kt
@@ -0,0 +1,126 @@
+package com.example.turecetaapp.presentation.authentication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.turecetaapp.R
+import com.example.turecetaapp.presentation.navigation.Screen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(
+    authViewModel: AuthViewModel,
+    navController: NavController,
+) {
+    val authState by authViewModel.authState.collectAsState()
+
+    val userEmail = when (authState) {
+        is AuthState.Authenticated -> authViewModel.auth.currentUser?.email ?: "Unknown"
+        else -> "Unknown"
+    }
+
+    val userName = userEmail.substringBefore('@').replaceFirstChar { it.uppercase() }
+
+    LaunchedEffect(authState) {
+        when (authState) {
+            is AuthState.Unauthenticated -> navController.navigate(Screen.LoginScreen)
+            else -> Unit
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Profile", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF273133),
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icons8_usuario_de_g_nero_neutro_100),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(bottom = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = userName,
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = userEmail,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = {
+                    authViewModel.signout()
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.padding(horizontal = 32.dp)
+            ) {
+                Text(text = "Cerrar Sesión", style = MaterialTheme.typography.labelLarge)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    navController.navigate(Screen.CategoriesList)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier.padding(horizontal = 32.dp)
+            ) {
+                Text(text = "Volver", style = MaterialTheme.typography.labelLarge)
+            }
+        }
+    }
+}
